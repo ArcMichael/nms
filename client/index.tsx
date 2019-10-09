126c21ba97fc5438861fe2d355710ea307c4f71b
Michael <mqiu@shlseven527.sephora-as.adam.net>
2019-10-09 16:33:53 +0800

    step4 finish

--- client/index.tsx
+++ client/index.tsx
@@ -3,11 +3,18 @@ import * as ReactDOM from "react-dom";
 import Hello from './containers/Hello';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import { Router } from "react-router";
+import { Route, BrowserRouter } from "react-router-dom";
+import Routes from "./routers"
+import { syncHistoryWithStore } from "react-router-redux";
 
 const store = configureStore();
+
 ReactDOM.render(
   <Provider store={store}>
-    <Hello name="123" />
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
---
+++ client/routers.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { Switch, Route } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import Hello from "./containers/Hello";
+import Hello2 from "./containers/Hello2";
+import { connect } from "react-redux";
+import { Dispatch } from 'redux';
+
+interface Props {
+    cName?: string;
+    changeName?: any;
+}
+
+class Routes extends React.Component<Props>{
+    render() {
+        return (
+            <Switch>
+                <Route path="/2" name="2" component={Hello2}></Route>
+                <Route component={Hello}></Route>
+            </Switch>
+        )
+    }
+}
+
+
+
+export default Routes;
