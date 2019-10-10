e501939af4779cb6484c870b324606d18eecd078
Michael <mqiu@shlseven527.sephora-as.adam.net>
2019-10-10 15:29:26 +0800

    step7 finish mixed router

--- client/routers.tsx
+++ client/routers.tsx
@@ -8,18 +8,63 @@ import { Dispatch } from 'redux';
 
 import Home from "./containers/Home";
 
-interface Props {
-    cName?: string;
-    changeName?: any;
-}
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
 
+import "./routers.css";
+
+interface Props{
+
+}
 class Routes extends React.Component<Props>{
+
+    constructor(props: Props){
+        super(props)
+    }
+
+    state = {
+        collapsed: false
+    }
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        })
+    }
+
     render() {
         return (
-            <Switch>
-                {/* <Route path="/2" name="2" component={Hello2}></Route> */}
-                <Route component={Home}></Route>
-            </Switch>
+            <Layout id="components-layout-trigger">
+                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                    <div className="logo" />
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Icon type="user" />
+                            <span>nav 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="video-camera" />
+                            <span>nav 2</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="upload" />
+                            <span>nav 3</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#fff', padding: 0 }}>
+                        <Icon
+                            className="trigger"
+                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                            onClick={this.toggle}
+                        />
+                    </Header>
+                    <Switch>
+                        <Route component={ Home } ></Route>
+                    </Switch>
+                </Layout>
+            </Layout>
         )
     }
 }
